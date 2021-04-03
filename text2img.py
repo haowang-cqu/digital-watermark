eb6d0ea512beebcef5f2e80854c4c6b7867956c8
iamwhcn <iamwh.cn@gmail.com>
2021-04-03 15:36:31 +0800

    Convert text to image

---
+++ text2img.py
@@ -0,0 +1,48 @@
+from PIL import Image,ImageFont,ImageDraw
+
+
+def textwrap(text, width, font):
+    """文本换行
+    """
+    lines = ['']
+    temp = width
+    for c in text:
+        w, _ = font.getsize(c)
+        if temp < w:
+            temp = width
+            lines.append('')
+        lines[-1] += c
+        temp -= w
+    return lines
+
+
+def text2img(text, width, height, mode='1', fontsize=30):
+    """文本转二值图
+    mode='1' 二值图
+    mode='RGB' RGB图
+    """
+    font = ImageFont.truetype("./fonts/YaHei Consolas Hybrid 1.12.ttf", fontsize)
+    lines = textwrap(text, width, font)
+    if mode == '1':
+        img = Image.new(mode, (width, height), 0)
+        dr = ImageDraw.Draw(img)
+        y = 0
+        for line in lines:
+            dr.text((0, y), line, font=font, fill=1)
+            y += 1.5 * fontsize # 1.5 倍行距
+        return img
+    elif mode == 'RGB':
+        img = Image.new(mode, (width, height), (0, 0, 0))
+        dr = ImageDraw.Draw(img)
+        y = 0
+        for line in lines:
+            dr.text((0, y), line, font=font, fill='#FFFFFF')
+            y += 1.5 * fontsize  # 1.5 倍行距
+        return img
+    else:
+        return None
+    
+
+if __name__=='__main__':
+    img = text2img(u"中文字符1234567890ABCDEFGHIJKLMNabcdefghijklmn", 300, 300, fontsize=50)
+    img.show()
